bc99a8019b86f0b0c51e9243fd4f4a173e9acc16
Ahmet <ahmetturk.pc@gmail.com>
2020-02-10 15:56:42 +0300

    Add navigation component

---
+++ app/src/main/java/com/ahmetroid/popularmovies/BaseFragment.kt
@@ -0,0 +1,19 @@
+package com.ahmetroid.popularmovies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BaseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+
+}
