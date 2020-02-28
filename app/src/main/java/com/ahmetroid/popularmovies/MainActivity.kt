3113e75e43472a4302d0203df5f7cdba84f7affb
Ahmet <ahmetturk.pc@gmail.com>
2020-02-28 15:05:29 +0300

    Fix app icon, navigation bar, detail view

--- app/src/main/java/com/ahmetroid/popularmovies/MainActivity.kt
+++ app/src/main/java/com/ahmetroid/popularmovies/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ahmetroid.popularmovies
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flag = window.decorView.systemUiVisibility
+        window.decorView.systemUiVisibility = flag or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     }
 }
